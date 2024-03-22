90fae9da2918a82c75ee4988989a7efa560bdd89
송대철 <81916321+daecheolsong@users.noreply.github.com>
2024-03-23 00:24:09 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 3.15 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,50 +1,68 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-       int [][] f = new int [friends.length][friends.length];
-        int [] rf = new int[friends.length];
-        int [] g = new int[friends.length];
+        int answer = 0;
+        
+        int[][] fm = new int[friends.length][friends.length];
+    
         Map<String, Integer> fMap = new HashMap<>();
-        for(int i = 0; i < friends.length; i++) {
+        
+        for(int i = 0; i < friends.length; i ++) {
             fMap.put(friends[i], i);
         }
+
         
-    /*
-     0 1 2 3
-   0 2 1 0 
-   1
-   2
-   3
-    */
         for(String gift : gifts) {
-            String [] lr = gift.split(" ");
-            int l = fMap.get(lr[0]);
-            int r = fMap.get(lr[1]);
-            f[l][r]++;
-            rf[l]++; // give
-            rf[r]--; // receive
+            StringTokenizer st = new StringTokenizer(gift);
+            
+            int from = fMap.get(st.nextToken());
+            int to = fMap.get(st.nextToken());
+            
+            fm[from][to]++;
         }
+        
+        
+        int[][] results = new int[friends.length][2];
+        
+        for(int i = 0; i < friends.length; i ++) {
+            int give = 0;
+            int given = 0;
+            for(int j = 0; j < friends.length; j++) {
+                give += fm[i][j];
+                given += fm[j][i];
+            }
+            results[i][0] = give;
+            results[i][1] = given;
+        }
+    
         
         for(int i = 0; i < friends.length; i++) {
-            for(int j = i + 1; j < friends.length; j ++) {
-                if(f[i][j] > f[j][i]) {
-                    g[i] ++;
+            int cnt = 0;
+            for(int j = 0; j < friends.length; j++ ){
+                if(i == j) {
                     continue;
                 }
-                if(f[i][j] == f[j][i]) {
-                    if(rf[i] > rf[j]) {
-                        g[i] ++;
-                    } else if(rf[i] < rf[j]){
-                        g[j]++;
+                
+                if(fm[i][j] != 0 || fm[j][i] != 0) {
+                    if(fm[i][j] > fm[j][i]) {
+                        cnt++;
+                    } else if(fm[i][j] == fm[j][i]) {
+                        if(results[i][0] - results[i][1] > results[j][0] - results[j][1]) {
+                            cnt++;
+                        }
                     }
-                    continue;
+                } else {
+                    if(results[i][0] - results[i][1] > results[j][0] - results[j][1]) {
+                        cnt++;
+                    }
                 }
-                g[j] ++;
             }
+            answer = Math.max(cnt, answer);
         }
-        Arrays.sort(g);
-        return g[friends.length-1];
         
+        
+        
+        return answer;
     }
+    
 }
