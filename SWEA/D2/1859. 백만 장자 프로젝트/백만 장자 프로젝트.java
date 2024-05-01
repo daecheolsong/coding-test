cdd9ac22c740617a2f1b47a64e6edcd47207e760
송대철 <81916321+daecheolsong@users.noreply.github.com>
2024-05-01 16:34:38 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 2,143 ms, Memory: 148,072 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -2,42 +2,43 @@ import java.util.*;
 
 class Solution
 {
-	public static void main(String args[]) throws Exception
-	{
-		
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-        
+    public static void main(String args[]) throws Exception
+    {
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
             long answer =  0;
-			int n = sc.nextInt();
+            int n = sc.nextInt();
             int [] arr = new int[n];
             for(int i = 0; i < n; i ++) {
                 arr[i] = sc.nextInt();
-			}
+            }
             int s = 0;
             while(s < arr.length) {
-         		int maxVal = 0;
+                int maxVal = 0;
                 int maxIdx = 0;
-                  for(int i = s; i < n ; i ++) {
-               		  if(arr[i] > maxVal) {
-                          maxVal = arr[i];
-                          maxIdx = i;
-                      }
-				  }
-                
+                for(int i = s; i < n ; i ++) {
+                    if(arr[i] > maxVal) {
+                        maxVal = arr[i];
+                        maxIdx = i;
+                    }
+                }
+
+
                 for(int i = s; i <= maxIdx; i ++) {
-                	if(maxVal > arr[i]) {
-                    	answer += (maxVal - arr[i]);
+                    if(maxVal > arr[i]) {
+                        answer += (maxVal - arr[i]);
                     }
                 }
                 s = maxIdx + 1;
             }
-            System.out.println("#" + test_case+ " "+ answer);
-            
+            System.out.println("#" + test_case + " " + answer);
+
         }
-	}
+    }
 }
