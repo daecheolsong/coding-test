a1ca1ee8b558de6da26c5daded34a56cc8dfc52c
송대철 <81916321+daecheolsong@users.noreply.github.com>
2024-10-28 23:04:25 +0900

    [Silver II] Title: 친구, Time: 148 ms, Memory: 16288 KB -BaekjoonHub

---
+++ 백준/Silver/1058. 친구/친구.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] arsg) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String [][] arr = new String[n][n];
+        for(int i = 0; i < n ; i ++) {
+            arr[i] = br.readLine().split("");
+        }
+        int answer = 0;
+        
+        for(int i = 0; i < n ; i ++) {
+            int cnt = 0;
+            for(int k = 0; k < n ; k ++) {
+                if(k == i) {
+                    continue;
+                }
+                for(int j = 0; j < n ; j ++) {
+                    if(arr[k][j].equals("Y")) {
+                        if(arr[i][j].equals(arr[k][j]) || arr[i][k].equals(arr[k][j])) {
+                            cnt++;
+                            break;
+                        }
+                    }
+                }
+            }
+            answer = Math.max(answer, cnt);
+        }
+        bw.write(answer + "\n");
+        bw.flush();
+    }
+}
