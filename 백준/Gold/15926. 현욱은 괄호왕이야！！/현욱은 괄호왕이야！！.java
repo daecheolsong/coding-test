110a223d149f6690d028a2d175718ac10bfc8ba7
송대철 <81916321+daecheolsong@users.noreply.github.com>
2025-01-04 22:11:36 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 216 ms, Memory: 24004 KB -BaekjoonHub

---
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		String str = br.readLine();
+		
+		Stack<Integer> s = new Stack<>();
+		s.push(-1);
+		
+		int maxL = 0;
+		
+		for(int i = 0; i < n; i ++) {
+			char c = str.charAt(i);
+			
+			if(c == '(') {
+				s.push(i);
+			}
+			
+			if(c == ')') {
+				s.pop();
+				if(!s.isEmpty()) {
+					maxL = Math.max(i - s.peek(), maxL);
+				} else {
+					s.push(i);
+				}
+			}
+		}
+		System.out.println(maxL);
+	}
+
+}
